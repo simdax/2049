15db992f00e9238fb660886445b2d6f2a3fa3e3c
simdax <simoncornaz@gmail.com>
2017-03-29 12:08:46 +0200

    add rules for fill

--- generate.js
+++ generate.js
@@ -19,12 +19,15 @@ function htmlTable(array) {
 	for (var i = 0; i < array.length ; i++) {
 		html += "<tr>";
 		for (var j = 0; j < array.length ; j++) {
-			var br = brightness(m[array.length*i+j]);
-			console.log(br);
-			if ((i%2!=0 && j%2!=0)||(i%2==0 && j%2==0)) {
-					html += "<td style='background-color: rgba("+br[2]+","+br[1]+","+br[0]*10+",1)'>"+array[i][j]+"</td>";
-			}else{				
-				html += "<td style='background-color: rgba("+br[0]+","+br[1]+","+br[2]+",1)'>"+array[i][j]+"</td>";
+			if (array[i][j]==0) {
+				html += "<td style='background-color: black'>"+array[i][j]+"</td>";
+			} else{
+				var br = brightness(m[array.length*i+j]);
+				if ((i%2!=0 && j%2!=0)||(i%2==0 && j%2==0)) {
+						html += "<td style='background-color: rgba("+br[2]+","+br[1]+","+br[0]*10+",1)'>"+array[i][j]+"</td>";
+				}else{				
+					html += "<td style='background-color: rgba("+br[0]+","+br[1]+","+br[2]+",1)'>"+array[i][j]+"</td>";
+				}
 			}
 
 		}	
--- recursive.js
+++ recursive.js
@@ -26,3 +26,17 @@ function fill(nb,table,begin=getCenter(nb),c=0){
 	fill(nb,table,{x:begin.x-1,y:begin.y},cc);
 	fill(nb,table,{x:begin.x,y:begin.y-1},cc);
 }
+
+// function fill(nb,table,begin=getCenter(nb),c=0){
+// 	var cc=c+1;	
+// //	if (cc>nb) {return};
+// 	if (cc>1 && cc>nb-1 ) {
+// 		table[begin.x][begin.y] += 1;
+// 		return
+// 	};
+// 	fill(nb,table,{x:begin.x+1,y:begin.y},cc);
+// 	fill(nb,table,{x:begin.x,y:begin.y+1},cc);
+// 	fill(nb,table,{x:begin.x-1,y:begin.y},cc);
+// 	fill(nb,table,{x:begin.x,y:begin.y-1},cc);
+// }
+
